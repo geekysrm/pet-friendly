2458a8a762f1ddc1ad5e6701bc6be3467ccda020
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2020-01-20 00:57:09 +0530

    update readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pet Friendly Restaurants`,
+    description: `Shows a list of pet friendly restaurants based on city.`,
+    author: `@geekysrm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
