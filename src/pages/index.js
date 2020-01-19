76f344c2f4c3bae952af0a8d1a95e13ab372aaed
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2020-01-20 01:49:05 +0530

    add styled components and other changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi pet friendly peeps!</h1>
-    <p>
-      Welcome to your new Gatsby site. In this project, we will list restaurants
-      that are pet-friendly.
-    </p>
+    {/* <SEO title="Home" /> */}
+    <h1>Hi pet friendly peeps!!</h1>
+    <p>In this project, we will list restaurants that are pet-friendly.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
