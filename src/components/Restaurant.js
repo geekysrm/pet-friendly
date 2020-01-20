e0cc05414dc3d7e6303ddd2a76f19340425a7e94
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2020-01-21 02:31:54 +0530

    major style improvements

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -11,30 +11,35 @@ const Restaurant = () => (
         alt="restaurant"
       />
     </ImageWrapper>
-    <h2>Domino's pizza</h2>
-    <div>PTT, High Grounds, Vasanth Nagar</div>
-    <div>10 am - 10pm</div>
-    <div>Rs. 200 for 2</div>
-    <div>North Indian, Chinese, South Indian</div>
+    <RestaurantDetails>
+      <h2>Domino's Pizza</h2>
+      <div>PTT, High Grounds, Vasanth Nagar</div>
+      <div>10 am - 10pm</div>
+      <div>Rs. 200 for 2</div>
+      <div>North Indian, Chinese, South Indian</div>
+    </RestaurantDetails>
   </RestaurantWrapper>
 )
 
 // Styled Components
 const RestaurantWrapper = styled.div`
   border: 1px solid black;
+  border-radius: 5px;
 `
 
 const ImageWrapper = styled.div`
-  padding: 5px;
+  /* padding: 5px; */
   display: flex;
   justify-content: center;
 
   img {
     height: 200px;
-    width: 400px;
+    width: 100%;
     max-width: 100%;
     object-fit: cover;
   }
 `
+
+const RestaurantDetails = styled.div``
 
 export default Restaurant
